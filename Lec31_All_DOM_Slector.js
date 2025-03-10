1b891d803c089cca901fabf97369b8bed3e8d790
Ahmadshafiq327 <ahmadshafiq6844@gmail.com>
2025-03-10 08:22:39 +0500

    Changes

---
+++ Lec31_All_DOM_Slector.js
@@ -0,0 +1,42 @@
+    // lec31 DOM Document Object Model
+
+
+
+
+
+console.log("DOM");
+
+
+
+
+// document.getElementById('title').getAttribute('class')  result = heading
+
+// document.getElementById('title').getAttribute('id') result = title
+
+// ﻿
+// const title = document.getElementById("title").setAttribute('class', 'ali') result = new class generate.
+
+
+// const title = document.getElementById("title") result isko variable main store krdia.
+
+
+// title.style.backgroundColor = 'blue' ===> result heading ka backgroundColor blue krdia.
+
+
+// title.style.color = 'pink'  =====> text ka color pink hogya.
+
+
+// title.style.padding = '10px' =====> padding ser krdi.
+
+// title.style.borderRadius = '20px'  ====> borderradius set krdia.title
+
+// isi trhan hum jo merzi chahain kr skty Han.
+
+
+
+
+// <h1 id="title" class="heading" style="background-color: blue; color: pink; padding: 10px; border-radius: 20px;">Dom learning on chai or code</h1>
+
+
+// sub changing krny ke bad...me ne browser k console main title likha tw mujhy yeh uper wali line likhi hoi agyi...iska mtlb me ne yeh sub chezain add ki han.
+
