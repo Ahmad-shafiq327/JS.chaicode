5b0691a7c17713f2bdc9c8ed66a0ba84c0b3c834
Ahmadshafiq327 <ahmadshafiq6844@gmail.com>
2025-03-16 05:59:54 +0500

    Changes

--- Lec31_All_DOM_Slector.js
+++ Lec31_All_DOM_Slector.js
@@ -9,6 +9,10 @@ console.log("DOM");
 
 
 
+// subsy phly browser main inspect kr ke console main document likh kr enter press krna hai...phr document dot likh kr jo merzi krna chaho Kro....const title = document.getElementById('title')...
+
+
+
 // document.getElementById('title').getAttribute('class')  result = heading
 
 // document.getElementById('title').getAttribute('id') result = title
@@ -39,4 +43,26 @@ console.log("DOM");
 
 
 // sub changing krny ke bad...me ne browser k console main title likha tw mujhy yeh uper wali line likhi hoi agyi...iska mtlb me ne yeh sub chezain add ki han.
+
+
+
+// textcontent method apply krny sy pura html content display ho jay ga...mtlb woh bi disply hoga jo humny css property sy hide kia houa hoga...but innertext method apply krny sy sirf content nazar ayga hide houa va data disply ni hoga..
+
+
+
+// jaisa ke me ne title.innerHTML apply kia result main is title ka text and all properties like that color paddding border-radious sub disply hoga.
+
+
+
+// document.querySelector('h1') likhny pr yeh first h1 ki details dedy ga...mtlb uski id and class and text...
+
+
+// const list = document.querySelector('li')....yeh sirf list ki first value ko axcess kry ga....q ke querySlector sirf first element ko target krta hai...
+
+
+
+
+
+
+
 
