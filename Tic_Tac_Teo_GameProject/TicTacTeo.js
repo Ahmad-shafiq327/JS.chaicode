150e5bb3bea2b6f20f859eb715af37469bc3acd9
Ahmadshafiq327 <ahmadshafiq6844@gmail.com>
2025-04-20 03:55:45 +0500

    Changes

--- Tic_Tac_Teo_GameProject/TicTacTeo.js
+++ Tic_Tac_Teo_GameProject/TicTacTeo.js
@@ -1,8 +1,10 @@
 // const ki value change ni hoti....but let ki value change hoti hai.
-const boxes = document.querySelectorAll(".box")
-const boddy = document.querySelector("body")
-const ResetBtn = document.querySelector("#reset-btn")
-
+let boxes = document.querySelectorAll(".box")
+let boddy = document.querySelector("body")
+let ResetBtn = document.querySelector("#reset-game")
+let newgame = document.querySelector('New-game')
+let winner = document.querySelector('msg-container')
+let msg = document.querySelector('#msg')
 
 // is game main by-default pattern ko pta hia ke phly X palayer ki bari hai and phr O player ki.
 
@@ -24,6 +26,8 @@ const winPatterns = [
 
 boxes.forEach((box) => {
     box.addEventListener('click', () => {
+        console.log("box was clicked");
+        
         if(turnO === true) {
             box.innerHTML = 'O'
             turnO = false
@@ -33,6 +37,53 @@ boxes.forEach((box) => {
             turnO = true
         }
         box.disabled = true
+
+        checkWinner()
     })
 })
+
+
+    const disableboxes = () => {
+
+        for(let item of boxes) {
+            item.disabled = true
+
+        }
+    }
+
+
+    const showWinner = (winner) => {
+        msg.innerHTML = `congradulation, Winner is ${winner}`;
+        msg-container.classList.remove("hide")
+        disableboxes();
+    }
+
+
+
+
+
+ // this is arrow function jiska name checkwinner hai.
+const checkWinner = () => {
+    for(let ele of winPatterns) {
+        
+        let pos1Val = boxes[ele[0]].innerHTML
+        let pos2Val = boxes[ele[1]].innerHTML
+        let pos3Val = boxes[ele[2]].innerHTML
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if(pos1Val === pos2Val && pos2Val === pos3Val) {
+                console.log("winner", pos1Val);
+                showWinner(pos1Val)
+
+                
+            }
+        }
+}
+}
+// checkWinner()---->	// Ek function hai jo check karega kisi player ne jeeta ya nahi.
+// for(let ele of winPatterns)---->	// Har winning pattern ko ek ek karke check karta hai.
+// console.log(ele[0], ele[1], ele[2])----->	// Woh teen positions print karta hai jo current pattern mein hain.
+// console.log(boxes[ele[0]].innerText, boxes[ele[1]].innerText, boxes[ele[2]].innerText);---->	Un boxes ke andar kya likha hai (X, O, ya blank), woh print karta hai.
+
+
 
