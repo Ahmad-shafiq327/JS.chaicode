8a78fc326c18ca4071358ecabb0e4306e4810395
Ahmadshafiq327 <ahmadshafiq6844@gmail.com>
2025-05-05 23:16:21 +0500

    Changes

---
+++ JSLecture41-to-50/Lec50_End_Story.js
@@ -0,0 +1,24 @@
+console.log("End story of javascript");
+
+console.log("Complete javascript series");
+
+
+console.log("hitaish sir chai or code");
+
+
+console.log("world best series of javascript series indepth knowledge");
+
+console.log("thanks hitaish sir");
+
+
+console.log("Now i will Start React.js");
+
+
+
+
+
+
+
+
+
+
