e95ab6b5d5e794ff485f508b0ef6382cba2e206e
Ahmadshafiq327 <ahmadshafiq6844@gmail.com>
2025-05-29 02:08:40 +0500

    Changes

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,26 +1,31 @@
+// axcess of inputs name from display
 let display = document.getElementById('display')
 
-
+// button click krny pr inputs main numbers ko display kia.
 function appendToDisplay(value) {
     display.value = display.value + value
+
 }
 
-function cleardisplay() {
-    display.value = ""
-}
-
-
-function DeleteDigit() {
-    display.value = display.value.slice(0, -1)
-}
-
-
+// apply eval method & need last five digits after ishariya.
 function CalculateResult() {
-    try {
-        display.value = eval(display.value)
-    }
+    try { // calculate DMAS and everything
+        display.value = eval(display.value).toFixed(5)
+
+    } // error from button & display in inputs
     catch(error) {
         display.value = "Error"
     }
 }
 
+// Empty inputs...at click c 
+function cleardisplay() {
+    display.value = ""
+}
+
+
+// remove last digit of inuputs
+function DeleteDigit() {
+    // remove last digits from inputs.
+    display.value = display.value.slice(0, -1)
+    }
